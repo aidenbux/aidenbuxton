511116a585f55a73057c0d31907db44888b26bdd
aidenbux <81707378+aidenbux@users.noreply.github.com>
2021-12-04 19:04:54 -0700

    Add files via upload

---
+++ Examples/sites/Skeletons/js/birds.js
@@ -0,0 +1,22 @@
+"use strict";
+
+VANTA.BIRDS({
+    el: "#aniBg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0xffffff,
+    color1: 0x0,
+    color2: 0x0,
+    birdSize: 1.50,
+    wingSpan: 16.00,
+    speedLimit: 3.00,
+    separation: 100.00,
+    alignment: 95.00,
+    cohesion: 11.00,
+    quantity: 2.00
+    })
